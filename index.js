42f4f077c80df0b9d7bc0b6281c6dde4b9b7066b
deathdevil0707 <shubhankaragarwal0707@gmail.com>
2024-10-29 16:52:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ import router from './routes/index.js';
 app.use(express.json())
 app.use(router)
 
+app.get("/",(req,res)=>{
+    try {
+        res.status(200).json({
+            status: 200,
+            message: "health is ok!"
+        });
+    } catch (error) {
+        res.status(200).json({
+            status: 500,
+            message: "flag0"
+        });
+    }
+})
 app.listen(7000, () => {
     console.log(`server is running on http://localhost:7000`);
     connectdb()
